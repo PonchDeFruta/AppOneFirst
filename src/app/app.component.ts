6050be802016f0290978da76119681d4b876751e
Pxnch <fmontesq1600@alumno.ipn.mx>
2024-02-10 18:16:25 -0600

    commit Inicial migracion de repositorio...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppOneFirst';
+  title = 'APPONE';
+
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  faqs = [
+    { question: 'Pregunta 1', answer: 'Respuesta a la pregunta 1.' },
+    { question: 'Pregunta 2', answer: 'Respuesta a la pregunta 2.' },
+    { question: 'Pregunta 3', answer: 'Respuesta a la pregunta 3.' },
+    { question: 'Pregunta 4', answer: 'Respuesta a la pregunta 4.' },
+    { question: 'Pregunta 5', answer: 'Respuesta a la pregunta 5.' },
+    { question: 'Pregunta 6', answer: 'Respuesta a la pregunta 6.' },
+    // Agrega más preguntas y respuestas según sea necesario
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
